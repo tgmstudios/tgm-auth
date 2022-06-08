6c647241d3b983bcb6864ec5791fe580f236433d
Aiden Johnson <aiden@tgmstudios.net>
2022-06-07 22:00:37 -0700

    Version 0.0.1
    Initial Commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const db = require('./lib/db.js')
+const email = require('./lib/email.js')
+const token = require('./lib/token.js')
+const user = require('./lib/user.js')
+const web = require('./lib/web.js')
+
+console.log("Initiating Authentication Server")
